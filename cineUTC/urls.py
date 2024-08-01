7cb850dd3990bbe68b33297e307bf0984e00856e
steven arequipa <steven.arequipa9883@utc.edu.ec>
2024-08-01 11:02:55 -0500

    actualizacion 6

--- cineUTC/urls.py
+++ cineUTC/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('Aplicaciones.Cartelera.urls'))
+    path('',include('Aplicaciones.Cartelera.urls')),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 #Validacion para visualizar los archivos subidos en modo DEV
